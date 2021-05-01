9300de57d23f34dc842593ef7e0d9f9b187a421e
rohangrge <rohangrge@gmail.com>
2021-05-02 03:07:04 +0530

    solved func bug

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def on_message(message):
                             if(piece == 'o'):
                                 await message.reply(discord.File("O final.gif"))
 
-                        if check_draw(cgarr, piece) == 1:
+                        if check_draw(cgarr) == 1:
                             flushRedis(str(message.channel.id))
                             await message.reply("GAME IS A DRAW")
                         if(check_win(cgarr, piece) == 0 and check_draw(cgarr, piece) == 0):
